f96b2803c63eb8b1219edf00c1ef1879e698426c
Luana <luana.marques@sunoresearch.com.br>
2022-03-22 15:42:14 -0300

    feat: add house card

--- src/App.js
+++ src/App.js
@@ -2,27 +2,43 @@ import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components';
 import Butom from '../src/components/butom';
+import HouseCard from './components/HouseCard';
 
 const CenterAlign = styled.div`
   text-align: center;
+  background: #f1f1f1;
 `;
 
 function App() {
   return (
     <CenterAlign>
+      <HouseCard priceText={'10.0000'}
+        title={'Residence'}
+        subtitle={'Kansas, UF'}
+        bedQuantity={'4'}
+        bathQuantity={'2'}
+        size={'123'}
+        href={'https://www.teclasap.com.br/wp-content/uploads/2009/09/house-1.jpg'} />
+         <HouseCard priceText={'10.0000'}
+        title={'Residence'}
+        subtitle={'Kansas, UF'}
+        bedQuantity={'4'}
+        bathQuantity={'2'}
+        size={'123'}
+        href={'https://www.teclasap.com.br/wp-content/uploads/2009/09/house-1.jpg'} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Butom/>
+        <Butom />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-           React
+          React
         </a>
       </header>
     </CenterAlign>
